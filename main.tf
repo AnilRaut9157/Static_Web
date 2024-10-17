bb51c2d776168495e42593169af2e3d6814ba9ab
anil <rautanil9157@gmail.com>
2024-10-17 14:43:55 +0530

    6th commit

--- main.tf
+++ main.tf
@@ -1,35 +1,25 @@
 resource "azurerm_resource_group" "myresourcegroupnew" {
-  name     = "my-resource-demo-new" # Name of your resource group
-  location = "East US"           # Location for your resources
+  name     = "my-resource-demo-new"
+  location = "East US"
 }
 
 resource "azurerm_storage_account" "mystorageaccountnew" {
-  name                     = "newstorageaccountdemo9" # Must be globally unique
+  name                     = "newstorageaccountdemo9"
   resource_group_name      = azurerm_resource_group.myresourcegroupnew.name
   location                 = azurerm_resource_group.myresourcegroupnew.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  static_website {
-    index_document    = "index.html"   # Set the index document for the static website
-  }
+  account_replication_type  = "LRS"
 }
 
-# resource "azurerm_storage_blob" "index_html" {
-#   name                   = "index.html"                # Name of the HTML file
-#   storage_account_name   = azurerm_storage_account.mystorageaccountnew.name
-#   storage_container_name = "$web"                     # Use the special container "$web" for static websites
-#   type                   = "Block"
-#   content_type           = "text/html"
-#   source                 = "C:/Users/rauta/OneDrive/Desktop/DemoHosting/index.html" # Path to your local HTML file
-# }
+resource "azurerm_storage_container" "web" {
+  name                  = "$web"
+  storage_account_name  = azurerm_storage_account.mystorageaccountnew.name
+}
 
 resource "azurerm_storage_blob" "index_html" {
   name                   = "index.html"
   storage_account_name   = azurerm_storage_account.mystorageaccountnew.name
-  storage_container_name = "$web"
+  storage_container_name = azurerm_storage_container.web.name
   type                   = "Block"
   source                 = "C:/Users/rauta/OneDrive/Desktop/DemoHosting/index.html"
 }
-
-
