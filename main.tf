ec4fd5702e0c6b119a1f45b179cf100ad96f1e6b
anil <rautanil9157@gmail.com>
2024-10-17 14:31:37 +0530

    changes the main and output

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "my-resource-demo" # Name of your resource group
+resource "azurerm_resource_group" "myresourcegroupnew" {
+  name     = "my-resource-demo-new" # Name of your resource group
   location = "East US"           # Location for your resources
 }
 
-resource "azurerm_storage_account" "mystorageaccount" {
-  name                     = "itsmystorageaccountdemo9" # Must be globally unique
-  resource_group_name      = azurerm_resource_group.myresourcegroup.name
-  location                 = azurerm_resource_group.myresourcegroup.location
+resource "azurerm_storage_account" "mystorageaccountnew" {
+  name                     = "newstorageaccountdemo9" # Must be globally unique
+  resource_group_name      = azurerm_resource_group.myresourcegroupnew.name
+  location                 = azurerm_resource_group.myresourcegroupnew.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
@@ -17,7 +17,7 @@ resource "azurerm_storage_account" "mystorageaccount" {
 
 resource "azurerm_storage_blob" "index_html" {
   name                   = "index.html"                # Name of the HTML file
-  storage_account_name   = azurerm_storage_account.mystorageaccount.name
+  storage_account_name   = azurerm_storage_account.mystorageaccountnew.name
   storage_container_name = "$web"                     # Use the special container "$web" for static websites
   type                   = "Block"
   content_type           = "text/html"
