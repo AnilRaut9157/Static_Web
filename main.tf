609c41be26a89dc8e580d05bc77cea6693bc8d3c
anil <rautanil9157@gmail.com>
2024-10-17 18:52:00 +0530

    completed

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
+# provider "azurerm" {
+#   features {}
+# }
+
+# Fetch the index.html file from a GitHub repository
+data "http" "index_html" {
+  url = "https://github.com/AnilRaut9157/DemoHosting.git/main/index.html"  # Update with your GitHub repo URL
+}
+
 resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "my-new-resource-group"  # Updated name of your resource group
-  location = "West US"                # Updated location for your resources
+  name     = "example-resources"
+  location = "West Europe"
 }
 
 resource "azurerm_storage_account" "mynewstorageaccount" {
-  name                     = "uniqueaccountdemo123"  # Updated unique storage account name
+  name                     = "examplestoracc"  # Must be globally unique
   resource_group_name      = azurerm_resource_group.myresourcegroup.name
   location                 = azurerm_resource_group.myresourcegroup.location
   account_tier             = "Standard"
@@ -12,15 +21,32 @@ resource "azurerm_storage_account" "mynewstorageaccount" {
 
   static_website {
     index_document = "index.html"   # Set the index document for the static website
+    # error_document = "404.html"      # Set the error document for the static website (optional)
   }
 }
 
-resource "azurerm_storage_blob" "index_html" {
-  name                   = "index.html"                              # Name of the HTML file
-  storage_account_name   = azurerm_storage_account.mynewstorageaccount.name
-  storage_container_name = azurerm_storage_container.web_container.name
-  type                   = "Block"
-  source                 = "C:/Users/rauta/OneDrive/Desktop/DemoHosting./index.html"              # Path to your local HTML file
+resource "azurerm_storage_container" "web" {
+  name                  = "$web"  # Use the special container for static websites
+  storage_account_name  = azurerm_storage_account.mynewstorageaccount.name
+  container_access_type = "private" # You may set this to 'public' if desired
 }
 
+resource "azurerm_storage_blob" "index_html" {
+  name                   = "index.html"                     # Name of the HTML file
+  storage_account_name   = azurerm_storage_account.mynewstorageaccount.name
+  storage_container_name = azurerm_storage_container.web.name
+  type                   = "Block"
+  source_content         = data.http.index_html.body
+}
 
+# resource "azurerm_storage_blob" "error_html" {
+#   name                   = "404.html"                     # Name of the error HTML file
+#   storage_account_name   = azurerm_storage_account.example.name
+#   storage_container_name = azurerm_storage_container.web.name
+#   type                   = "Block"
+#   source                 = "C:/path/to/your/local/404.html"  # Update with the path to your local error HTML file
+# }
+
+output "static_website_url" {
+  value = azurerm_storage_account.mynewstorageaccount.primary_web_endpoint  # Output the static website URL
+}
