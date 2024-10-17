72453dc4efa1a4a258e83c408213c6cd2ae7da29
anil <rautanil9157@gmail.com>
2024-10-17 19:46:43 +0530

    9th commit

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 
 # Create a Resource Group
 resource "azurerm_resource_group" "azurergforstaticweb" {
-  name     = "it-is-my-new-resources"  # Name of the resource group
+  name     = "it-is-my-new-resources1"  # Name of the resource group
   location = "East US"                  # Location for the resource group
 }
 
 # Create a Storage Account with Static Website Hosting Enabled
 resource "azurerm_storage_account" "myazurestorage" {
-  name                     = "mystorageacctunique12345"  # Unique storage account name
+  name                     = "mystorageacctunique54321"  # Unique storage account name
   resource_group_name      = azurerm_resource_group.azurergforstaticweb.name
   location                 = azurerm_resource_group.azurergforstaticweb.location
   account_tier             = "Standard"                   # Account tier
