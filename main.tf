2333e642c24f0d8b1a5e9d7e33ddf1381c57a5d0
anil <rautanil9157@gmail.com>
2024-10-17 14:53:57 +0530

    final

--- main.tf
+++ main.tf
@@ -1,25 +1,24 @@
-resource "azurerm_resource_group" "myresourcegroupnew" {
-  name     = "my-resource-demo-new"
-  location = "East US"
+resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "my-resource-demo-new"  # Updated name of your resource group
+  location = "West US"                # Updated location for your resources
 }
 
-resource "azurerm_storage_account" "mystorageaccountnew" {
-  name                     = "newstorageaccountdemo9"
-  resource_group_name      = azurerm_resource_group.myresourcegroupnew.name
-  location                 = azurerm_resource_group.myresourcegroupnew.location
+resource "azurerm_storage_account" "mystorageaccount" {
+  name                     = "uniqueaccountdemo123"  # Updated unique storage account name
+  resource_group_name      = azurerm_resource_group.myresourcegroup.name
+  location                 = azurerm_resource_group.myresourcegroup.location
   account_tier             = "Standard"
-  account_replication_type  = "LRS"
-}
+  account_replication_type = "LRS"
 
-resource "azurerm_storage_container" "web" {
-  name                  = "$web"
-  storage_account_name  = azurerm_storage_account.mystorageaccountnew.name
+  static_website {
+    index_document = "index.html"   # Set the index document for the static website
+  }
 }
 
 resource "azurerm_storage_blob" "index_html" {
-  name                   = "index.html"
-  storage_account_name   = azurerm_storage_account.mystorageaccountnew.name
-  storage_container_name = azurerm_storage_container.web.name
+  name                   = "index.html"                # Name of the HTML file
+  storage_account_name   = azurerm_storage_account.mystorageaccount.name
+  storage_container_name = "$web"                     # Use the special container "$web" for static websites
   type                   = "Block"
-  source                 = "C:/Users/rauta/OneDrive/Desktop/DemoHosting/index.html"
+  source                 = "D:/HtmlPrograms/index.html" # Path to your local HTML file
 }
