3790c15e307499a6bed31f48a7ffca9c877b6193
anil <rautanil9157@gmail.com>
2024-10-17 19:55:45 +0530

    ok done

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 
 # Create a Resource Group
 resource "azurerm_resource_group" "azurergforstaticweb" {
-  name     = "it-is-my-new-resources1"  # Name of the resource group
+  name     = "it-is-my-new-resources-ii"  # Name of the resource group
   location = "East US"                  # Location for the resource group
 }
 
 # Create a Storage Account with Static Website Hosting Enabled
 resource "azurerm_storage_account" "myazurestorage" {
-  name                     = "mystorageacctunique54321"  # Unique storage account name
+  name                     = "mystorageacctunique09"  # Unique storage account name
   resource_group_name      = azurerm_resource_group.azurergforstaticweb.name
   location                 = azurerm_resource_group.azurergforstaticweb.location
   account_tier             = "Standard"                   # Account tier
@@ -18,17 +18,3 @@ resource "azurerm_storage_account" "myazurestorage" {
   }
 }
 
-# resource "azurerm_storage_container" "web_container" {
-#   name                  = "$web"                       # Use the special container name for static websites
-#   storage_account_name  = azurerm_storage_account.myazurestorage.name
-#   container_access_type = "private"                    # Set access type to private
-# }
-
-# Upload the index.html file to the $web container
-resource "azurerm_storage_blob" "index_html" {
-  name                   = "index.html"                              # Name of the HTML file
-  storage_account_name   = azurerm_storage_account.myazurestorage.name
-  storage_container_name = "$web"
-  type                   = "Block"                                   # Blob type
-  source                 = "C:\\Users\\rauta\\OneDrive\\Desktop\\DemoHosting\\index.html"            # Path to your local index.html file
-}
